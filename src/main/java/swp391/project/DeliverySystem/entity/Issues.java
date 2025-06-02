b3e084790c6ca20561d74002eca423f8112e168a
MissPinkDiamond <pinkdiamond2k5@gmail.com>
2025-06-02 13:39:50 +0700

    Thêm bảng issues.java

--- src/main/java/swp391/project/DeliverySystem/entity/Issues.java
+++ src/main/java/swp391/project/DeliverySystem/entity/Issues.java
@@ -1,5 +1,47 @@
 package swp391.project.DeliverySystem.entity;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "issues")
 public class Issues {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "IncidentCode")
+    private String incidentCode;
+
+    @Column(name = "OrderID")
+    private Long orderID;
+
+    @Column(name = "CustomerID")
+    private Long customerID;
+
+    @Column(name = "IncidentDate")
+    private LocalDate incidentDate;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Resolution")
+    private String resolution;
+
+    @Column(name = "Status")
+    private String status;
 }
