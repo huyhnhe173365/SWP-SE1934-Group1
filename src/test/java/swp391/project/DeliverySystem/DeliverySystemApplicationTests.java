9629694970a5328cf94159cef809ecdf908ee75b
MissPinkDiamond <pinkdiamond2k5@gmail.com>
2025-06-04 09:37:26 +0700

    .

--- src/test/java/swp391/project/DeliverySystem/DeliverySystemApplicationTests.java
+++ src/test/java/swp391/project/DeliverySystem/DeliverySystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DelierySystemApplicationTests {
+class DeliverySystemApplicationTests {
 
 	@Test
 	void contextLoads() {
