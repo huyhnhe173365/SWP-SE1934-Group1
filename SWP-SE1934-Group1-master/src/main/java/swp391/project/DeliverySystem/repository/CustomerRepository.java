637fd9f5df91a3aea2616796e1bab3d38e6353de
MissPinkDiamond <pinkdiamond2k5@gmail.com>
2025-06-04 08:36:34 +0700

    Bổ sung DTO

--- SWP-SE1934-Group1-master/src/main/java/swp391/project/DeliverySystem/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package swp391.project.DeliverySystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import swp391.project.DeliverySystem.entity.Customers;
-
-public interface CustomerRepository extends JpaRepository<Customers, Long>{
-    
-}
